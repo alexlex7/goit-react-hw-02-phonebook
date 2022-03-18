4fe49175613087a38a57ef0f56882b139fd965da
alexlex7 <alekslosev7@gmail.com>
2022-03-18 14:44:22 +0200

    fixed contact list and filter

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,38 +1,36 @@
 import s from './ContactsList.module.css';
-import { Component } from 'react';
-import PropTypes from 'prop-types';
 import ContactsListElement from './ContactListElement';
-class ContactsList extends Component {
-  static propTypes = {
-    contacts: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string,
-        name: PropTypes.string,
-        number: PropTypes.string,
-      })
-    ),
-    filter: PropTypes.string.isRequired,
-    onDeleteContact: PropTypes.func.isRequired,
-  };
-  render() {
-    const { contacts, filter, onDeleteContact } = this.props;
-    const filteredContacts = contacts.filter(({ name }) =>
-      name.toLowerCase().includes(filter)
-    );
-    return (
-      <ul className={s.list}>
-        {filteredContacts.map(({ id, name, number }) => (
-          <ContactsListElement
-            key={id}
-            name={name}
-            number={number}
-            id={id}
-            onDeleteContact={onDeleteContact}
-          />
-        ))}
-      </ul>
-    );
-  }
+import PropTypes from 'prop-types';
+
+function ContactsList({ contacts, filter, onDeleteContact }) {
+  const filteredContacts = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(filter)
+  );
+  return (
+    <ul className={s.list}>
+      {filteredContacts.map(({ id, name, number }) => (
+        <ContactsListElement
+          key={id}
+          name={name}
+          number={number}
+          id={id}
+          onDeleteContact={onDeleteContact}
+        />
+      ))}
+    </ul>
+  );
 }
+
+ContactsList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      number: PropTypes.string,
+    })
+  ),
+  filter: PropTypes.string.isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
 
 export default ContactsList;
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,26 +1,18 @@
-import { Component } from 'react';
 import s from './Filter.module.css';
 import PropTypes from 'prop-types';
 
-class Filter extends Component {
-  static propTypes = {
-    handleChange: PropTypes.func.isRequired,
-    filter: PropTypes.string.isRequired,
-  };
-  render() {
-    const { handleChange, filter } = this.props;
-    return (
-      <label className={s.filterLabel}>
-        Find contacts by name
-        <input
-          type="text"
-          value={filter}
-          name="filter"
-          onChange={handleChange}
-        />
-      </label>
-    );
-  }
+function Filter({ handleChange, filter }) {
+  return (
+    <label className={s.filterLabel}>
+      Find contacts by name
+      <input type="text" value={filter} name="filter" onChange={handleChange} />
+    </label>
+  );
 }
+
+Filter.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
+};
 
 export default Filter;
